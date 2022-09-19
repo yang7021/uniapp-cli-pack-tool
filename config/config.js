5f9ce91bde9fe4f3cc7494e7844ad7b1e2f5fbf5
JOJO <1097085674@qq.com>
2022-09-19 16:22:22 +0800

    修改配置目录注释

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ export default {
     cliExe: '`D:/HBuilderX/cli.exe HBuilderX根目录的cli.exe位置`',
     buildItemDir: '`D:/项目 项目根目录/`',
     buildUnpackageDir: '`D:/项目/unpackage/release/apk/ app生成的目录`',
-    publishDir: '`D:/hhb/app_update/ app输入目录`', 
+    publishDir: '`D:/app_update_dirs/ app输入目录`', 
     piblishConfig:[
         {
             name: '应用名a,打32位包', // 应用名
